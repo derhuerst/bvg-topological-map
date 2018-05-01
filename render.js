2e2d9e36df2d443089db4f63a1b1fe07cc54cf4c
Jannis R <mail@jannisr.de>
2018-05-01 21:08:15 +0200

    allow overriding the render fns

--- render.js
+++ render.js
@@ -22,7 +22,7 @@ const common = `
 }
 `
 
-const renderStyles = (data) => {
+const renderStyles = (h, opt, data) => {
 	let css = common
 
 	for (let line of Object.keys(data.lines)) {
@@ -34,7 +34,7 @@ const renderStyles = (data) => {
 	return css
 }
 
-const renderDefs = (h, data) => {
+const renderDefs = (h, opt, data) => {
 	const defs = []
 	for (let id of Object.keys(data.labels)) {
 		const label = data.labels[id]
@@ -55,73 +55,81 @@ const renderDefs = (h, data) => {
 	return defs
 }
 
-const renderLabel = (h, id, [x, y]) => {
-	return h('use', {
-		class: 'label ' + id,
-		'xlink:href': '#label-' + id,
-		href: '#label-' + id,
-		transform: `translate(${x}, ${y})`
-	})
-}
+const labelProps = (h, id, [x, y]) => ({
+	class: 'label ' + id,
+	'xlink:href': '#label-' + id,
+	href: '#label-' + id,
+	transform: `translate(${x}, ${y})`
+})
 
-const renderLabels = (h, data) => {
+const renderLabels = (h, opt, data) => {
 	const r = []
 	for (let id of Object.keys(data.labels)) {
 		const label = data.labels[id]
 		for (let position of label.positions) {
-			r.push(renderLabel(h, id, position))
+			r.push(h('use', opt.labelProps(h, id, position)))
 		}
 	}
 	return r
 }
 
-const renderLine = (h, id, line) => {
-	return h('path', {
-		id: 'line-' + id,
-		class: 'line ' + id,
-		d: line.shape
-	})
-}
+const lineProps = (h, id, line) => ({
+	id: 'line-' + id,
+	class: 'line ' + id,
+	d: line.shape
+})
 
-const renderLines = (h, data) => {
+const renderLines = (h, opt, data) => {
 	const r = []
 	for (let id of Object.keys(data.lines)) {
 		const line = data.lines[id]
-		r.push(renderLine(h, id, line))
+		r.push(h('path', opt.lineProps(h, id, line)))
 	}
 	return r
 }
 
-const renderStation = (h, id, station) => {
-	return h('path', {
-		id: 'station-' + id,
-		'data-id': id,
-		class: 'station ' + station.lines.join(' ') + (station.wifi ? ' wifi' : ''),
-		d: station.shape
-	})
-}
+const stationProps = (h, id, station) => ({
+	id: 'station-' + id,
+	'data-id': id,
+	class: 'station ' + station.lines.join(' ') + (station.wifi ? ' wifi' : ''),
+	d: station.shape
+})
 
-const renderInterchanges = (h, data) => {
+const renderInterchanges = (h, opt, data) => {
 	const r = []
 	for (let id of Object.keys(data.stations)) {
 		const station = data.stations[id]
 		if (!station.interchange) continue
-		r.push(renderStation(h, id, station))
+		r.push(h('path', opt.stationProps(h, id, station)))
 	}
 	return r
 }
 
-const renderStops = (h, data) => {
+const renderStops = (h, opt, data) => {
 	const r = []
 	for (let id of Object.keys(data.stations)) {
 		const station = data.stations[id]
 		if (station.interchange) continue
-		r.push(renderStation(h, id, station))
+		r.push(h('path', opt.stationProps(h, id, station)))
 	}
 	return r
 }
 
-const render = (h, data) => {
+const defaults = {
+	renderStyles,
+	renderDefs,
+	labelProps,
+	renderLabels,
+	lineProps,
+	renderLines,
+	stationProps,
+	renderInterchanges,
+	renderStops
+}
+
+const render = (h, data, opt = {}) => {
+	opt = Object.assign({}, defaults, opt)
+
 	return h('svg', {
 		xmlns: 'http://www.w3.org/2000/svg',
 		'xmlns:xlink': 'http://www.w3.org/1999/xlink',
@@ -129,17 +137,18 @@ const render = (h, data) => {
 		height: data.height,
 		viewBox: `0 0 ${data.width} ${data.height}`
 	}, [
-		h('style', {}, renderStyles(data)),
-		h('defs', {}, renderDefs(h, data)),
-		h('g', {id: 'labels'}, renderLabels(h, data)),
-		h('g', {id: 'lines'}, renderLines(h, data)),
+		h('style', {}, opt.renderStyles(h, opt, data)),
+		h('defs', {}, opt.renderDefs(h, opt, data)),
+		h('g', {id: 'labels'}, opt.renderLabels(h, opt, data)),
+		h('g', {id: 'lines'}, opt.renderLines(h, opt, data)),
 		h('g', {id: 'stations'}, [
 			h('g', {id: 'interchanges'}, [
-				renderInterchanges(h, data)
+				opt.renderInterchanges(h, opt, data)
 			]),
-			...renderStops(h, data)
+			...opt.renderStops(h, opt, data)
 		])
 	])
 }
 
+render.defaults = defaults
 module.exports = render
